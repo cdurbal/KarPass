6955a81e377eba9c62e0eb65f0d42aace0f376f4
cdurbal <cdurbal@cadaero.com>
2020-10-10 14:32:26 +0200

    feat: complete contract

--- contracts/KarPassport.sol
+++ contracts/KarPassport.sol
@@ -37,31 +37,41 @@ contract KarPassport {
         mapping(uint => Event) data;
     }
 
+    struct MappingWaitingEvent {
+        uint256 size;
+        mapping(uint => Event) data;
+        MappingIndex index;
+    }
+
+    struct MappingIndex {
+        uint size;
+        mapping(uint => uint256) data;
+    }
+
     struct MappingOwner {
         uint size;
         mapping(uint => Owner) data;
     }
 
-    
-    
     enum EventType { Insurance, Repairing, TechnicalControl, Accident, Upgrade }
 
     struct Event {
         EventType typeEvent;
-        string description; 
-        address contributor; 
+        string description;
+        address sender;
         string interventions;
         uint256 date;
-        uint price; 
+        uint price;
         string currency;
         uint256 expirationDate;
+        bool exists;
     }
 
     struct Owner {
         address _owner;
         uint256 acquisitionDate;
     }
-    
+
     struct CarIdentity{
         // car info
         string  name;
@@ -78,28 +88,28 @@ contract KarPassport {
         uint numberOfPlace;
         uint  power;
         Mapping  options;
-        
+
         string  numberPlate;
     }
-    
+
     struct Passport {
         address owner;
         CarIdentity identity;
-    
+
         //car variable
         uint256  km;
         uint256  technicalControlExpirationDate;
         uint256  insuranceExpirationDate;
-        
+
         //contract variable
         uint256 balance;
-        
+
         //car event
         MappingEvent events;
-        MappingEvent toBeValidatedEvents;
+        MappingWaitingEvent waitingEvents;
         MappingOwner historyOwners;
-        
-        bool exist;
+
+        bool exists;
     }
 
 
@@ -115,12 +125,23 @@ contract KarPassport {
     }
 
     /*
+        Create a modifier that throws an error if not the event creator.
+    */
+    modifier requireEventCreator(uint256 idPassport, uint256 idEvent) {
+        require(
+            _passport[idPassport].waitingEvents.data[idEvent].sender >= msg.sender,
+            "No token allowed"
+        );
+        _;
+    }
+
+    /*
         Create a modifier that throws an error if the msg.sender has not the require token number.
     */
     modifier requireToken(uint numToken) {
         require(
-            numToken<=_balance[msg.sender], 
-            "Not enought token"
+            numToken<=_balance[msg.sender],
+            "Not enougth token"
         );
         _;
     }
@@ -130,30 +151,40 @@ contract KarPassport {
     */
     modifier requireLockedToken(uint numToken) {
         require(
-            numToken<=_lockedBalance[msg.sender], 
-            "Not enought locked token"
+            numToken<=_lockedBalance[msg.sender],
+            "Not enougth locked token"
         );
         _;
     }
-    
-    
+
+    /*
+        Create a modifier that throws an error if the msg.sender has not the require token number.
+    */
+    modifier requirePassportToken(uint256 idPassport, uint numToken) {
+        require(
+            _passport[idPassport].balance>=numToken,
+            "Not enougth token on passport"
+        );
+        _;
+    }
+
     /*
         Create a modifier that throws an error if the msg.sender is not the owner.
     */
     modifier requireOwner(uint256 passportId) {
         require(
-            _passport[passportId].owner == msg.sender, 
+            _passport[passportId].owner == msg.sender,
             "Not owner"
         );
         _;
     }
-    
+
     /*
         Create a modifier that throws an error if the acquirer has not enougth token.
     */
     modifier requirePassportPrice(address acquirer){
         require(
-            PASSPORT_PRICE <=_balance[acquirer] + KarToken(_tokenContractAddr).allowance(acquirer, address(this)), 
+            PASSPORT_PRICE<=_balance[acquirer]+KarToken(_tokenContractAddr).allowance(acquirer, address(this)),
             "Not enought token"
         );
         _;
@@ -164,7 +195,7 @@ contract KarPassport {
     */
     modifier requireAvailableToken(address sender, uint numToken){
         require(
-            numToken <=_balance[sender] + KarToken(_tokenContractAddr).allowance(sender, address(this)), 
+            numToken<=_balance[sender]+KarToken(_tokenContractAddr).allowance(sender, address(this)),
             "Not enought token"
         );
         _;
@@ -175,7 +206,7 @@ contract KarPassport {
     */
     modifier requireExists(uint256 passportId) {
         require(
-            _passport[passportId].exist, 
+            _passport[passportId].exists,
             "the passport does not exist"
         );
         _;
@@ -184,9 +215,20 @@ contract KarPassport {
     /*
         Create a modifier that throws an error if event does not exist.
     */
-    modifier requireEventExists(uint256 passportId, uint index) {
+    modifier requireEventExists(uint256 passportId, uint256 idEvent) {
         require(
-            _passport[passportId].events.size > 0 && _passport[passportId].events.size <= index, 
+            _passport[passportId].events.size > 0 && _passport[passportId].events.data[idEvent].exists,
+            "the passport does not exist"
+        );
+        _;
+    }
+
+    /*
+        Create a modifier that throws an error if waiting event does not exist.
+    */
+    modifier requireWaitingEventExists(uint256 passportId, uint idEvent) {
+        require(
+            _passport[passportId].waitingEvents.size > 0 && _passport[passportId].waitingEvents.data[idEvent].exists,
             "the passport does not exist"
         );
         _;
@@ -198,20 +240,25 @@ contract KarPassport {
     */
     constructor(address tokenContractAddr)
     public
-    {  
+    {
         _tokenContractAddr = tokenContractAddr;
     }
-    
+
     /*
         createPassport
     */
-    function createPassport(string memory name, string memory id, string memory brand, string memory modele, uint256 year, string memory numberPlate)
+    function createPassport(string memory name,
+                            string memory id,
+                            string memory brand,
+                            string memory modele,
+                            uint256 year,
+                            string memory numberPlate)
     public returns (uint256)
     {
         require(_balance[msg.sender] >= PASSPORT_PRICE, "Not enought token");
 
         _balance[msg.sender] = _balance[msg.sender].sub(PASSPORT_PRICE);
-        
+
         _totalPassport++;
         _passport[_totalPassport].owner = msg.sender;
         _passport[_totalPassport].identity.name = name;
@@ -221,22 +268,22 @@ contract KarPassport {
         _passport[_totalPassport].identity.year = year;
         _passport[_totalPassport].identity.numberPlate = numberPlate;
         _passport[_totalPassport].balance = PASSPORT_PRICE;
-        _passport[_totalPassport].exist = true;
-        
+        _passport[_totalPassport].exists = true;
+
         //Update search with address index
         _passportIdentifiants[msg.sender].size++;
         uint indexPassportId = _passportIdentifiants[msg.sender].size;
         _passportIdentifiants[msg.sender].data[indexPassportId] = _totalPassport;
-        
+
         return _totalPassport;
     }
-    
+
     /**
      * deletePassport
      */
-    function deletePassport(uint256 passportId) 
+    function deletePassport(uint256 passportId)
     public requireOwner(passportId) returns (bool){
-        
+
         _balance[msg.sender] = _balance[msg.sender].add(_passport[passportId].balance);
         delete(_passport[passportId]);
 
@@ -267,15 +314,15 @@ contract KarPassport {
 
         return true;
     }
-    
+
     /**
      * transferAllowedToken
      */
-    function transferAllowedToken() 
+    function transferAllowedToken()
     public returns (bool) {
         return _transferAllowedToken(msg.sender);
     }
-    
+
     /**
      * _transferAllowedToken
      */
@@ -288,7 +335,7 @@ contract KarPassport {
         emit TransfertToken(sender, address(this), numToken, _balance[sender]);
         return true;
     }
-    
+
     /**
      * withdrawToken
      */
@@ -299,38 +346,38 @@ contract KarPassport {
         _balance[msg.sender] = _balance[msg.sender].sub(numToken);
         return true;
     }
-    
+
     /**
      * transferPassport
      */
     function transferPassport(uint256 passportId, address acquirer)
     public requireOwner(passportId) requirePassportPrice(acquirer) returns (bool){
-        
+
         if(_balance[acquirer] < PASSPORT_PRICE){
             _transferAllowedToken(acquirer);
         }
         _balance[msg.sender] = _balance[msg.sender].add(_passport[passportId].balance);
-        
+
         _balance[acquirer] = _balance[acquirer].sub(PASSPORT_PRICE);
         _passport[passportId].balance = PASSPORT_PRICE;
         _passport[passportId].owner = acquirer;
-        
+
         return true;
     }
-    
+
     /**
      * passportIdentifiants
      */
     function passportIdentifiants()
-    public view returns (uint[] memory){
-        uint[] memory memoryArray = new uint[](_passportIdentifiants[msg.sender].size);
+    public view returns (uint256[] memory){
+        uint256[] memory memoryArray = new uint256[](_passportIdentifiants[msg.sender].size);
 
         for(uint i = 0; i < _passportIdentifiants[msg.sender].size; i++) {
-            memoryArray[i] = _passportIdentifiants[msg.sender].data[i];
+            memoryArray[i] = _passportIdentifiants[msg.sender].data[i+1];
         }
         return memoryArray;
     }
-    
+
     /**
      * @dev Returns the number of event
      */
@@ -341,12 +388,12 @@ contract KarPassport {
     /**
         passportEvent
      */
-    function passportEvent(uint256 idPassport, uint eventIndex) 
-    public view requireExists(idPassport) requireEventExists(idPassport, eventIndex) 
+    function passportEvent(uint256 idPassport, uint eventIndex)
+    public view requireExists(idPassport) requireEventExists(idPassport, eventIndex)
     returns (EventType, string memory, address, string memory, uint256, uint, string memory, uint256) {
          return (_passport[idPassport].events.data[eventIndex].typeEvent,
                 _passport[idPassport].events.data[eventIndex].description,
-                _passport[idPassport].events.data[eventIndex].contributor,
+                _passport[idPassport].events.data[eventIndex].sender,
                 _passport[idPassport].events.data[eventIndex].interventions,
                 _passport[idPassport].events.data[eventIndex].date,
                 _passport[idPassport].events.data[eventIndex].price,
@@ -355,39 +402,135 @@ contract KarPassport {
     }
 
     /**
+    * passportWaitingEvents
+    */
+    function passportWaitingEvents(uint256 idPassport, uint eventIndex)
+    public view requireExists(idPassport) requireWaitingEventExists(idPassport, eventIndex)
+    returns (EventType, string memory, address, string memory, uint256, uint, string memory, uint256) {
+         return (_passport[idPassport].waitingEvents.data[eventIndex].typeEvent,
+                _passport[idPassport].waitingEvents.data[eventIndex].description,
+                _passport[idPassport].waitingEvents.data[eventIndex].sender,
+                _passport[idPassport].waitingEvents.data[eventIndex].interventions,
+                _passport[idPassport].waitingEvents.data[eventIndex].date,
+                _passport[idPassport].waitingEvents.data[eventIndex].price,
+                _passport[idPassport].waitingEvents.data[eventIndex].currency,
+                _passport[idPassport].waitingEvents.data[eventIndex].expirationDate);
+    }
+
+    /**
+    * passportWaitingEventsIDs
+    */
+    function passportWaitingEventsIDs(uint256 idPassport)
+    public view requireExists(idPassport)
+    returns (uint256[] memory){
+        uint256[] memory memoryArray = new uint256[](_passport[idPassport].waitingEvents.index.size);
+
+        for(uint i = 0; i < _passport[idPassport].waitingEvents.index.size; i++) {
+            if(msg.sender==_passport[idPassport].waitingEvents.data[i+1].sender ||
+                msg.sender==_passport[idPassport].owner){
+                memoryArray[i] = _passport[idPassport].waitingEvents.index.data[i+1];
+            }
+        }
+        return memoryArray;
+    }
+
+    /**
     * createEvent
     */
-    function createEvent(uint256 idPassport, 
+    function createEvent(uint256 idPassport,
                             EventType typeEvent,
                             string memory description,
                             string memory interventions,
                             uint256 date,
                             uint price,
                             string memory currency,
-                            uint256 expirationDate) 
+                            uint256 expirationDate)
     public requireToken(TOKEN_EVENT_LOCK) returns (bool){
         _lockToken(msg.sender, TOKEN_EVENT_LOCK);
-        _passport[idPassport].toBeValidatedEvents.size++;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].typeEvent=typeEvent;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].description=description;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].contributor=msg.sender;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].interventions=interventions;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].date=date;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].price=price;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].currency=currency;
-        _passport[idPassport].toBeValidatedEvents.data[_passport[idPassport].toBeValidatedEvents.size].expirationDate=expirationDate;
+
+        _passport[idPassport].waitingEvents.size++;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].typeEvent = typeEvent;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].description = description;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].sender = msg.sender;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].interventions = interventions;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].date = date;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].price = price;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].currency = currency;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].expirationDate = expirationDate;
+        _passport[idPassport].waitingEvents.data[_passport[idPassport].waitingEvents.size].exists = true;
+
+        _passport[idPassport].waitingEvents.index.size++;
+        uint256 idEvent = _passport[idPassport].waitingEvents.size;
+        _passport[idPassport].waitingEvents.index.data[_passport[idPassport].waitingEvents.index.size] = idEvent;
         return true;
     }
 
     /**
     * approveEvent
     */
-    function approveEvent(uint256 idPassport, uint index)
-    public requireToken(TOKEN_EVENT_REWARD) returns (bool)
+    function approveEvent(uint256 idPassport, uint idEvent)
+    public requireOwner(idPassport) requirePassportToken(idPassport, TOKEN_EVENT_REWARD) returns (bool)
     {
-        
-        _unlockToken(_passport[idPassport].toBeValidatedEvents.data[index].contributor, TOKEN_EVENT_LOCK);
+         address eventSender = _passport[idPassport].waitingEvents.data[idEvent].sender;
+        _unlockToken(eventSender, TOKEN_EVENT_LOCK);
+        _passport[idPassport].balance = _passport[idPassport].balance.sub(TOKEN_EVENT_REWARD);
+        _balance[eventSender] = _balance[eventSender].add(TOKEN_EVENT_REWARD);
+
+        _passport[idPassport].events.size++;
+        _passport[idPassport].events.data[_passport[idPassport].events.size] = _passport[idPassport].waitingEvents.data[idEvent];
+
+        _deleteWaitingEvent(idPassport, idEvent);
         return true;
+    }
+
+    /**
+    * approveEvent
+    */
+    function dismissEvent(uint256 idPassport, uint idEvent)
+    public requireOwner(idPassport) returns (bool)
+    {
+        _unlockToken(_passport[idPassport].waitingEvents.data[idEvent].sender, TOKEN_EVENT_LOCK);
+        _deleteWaitingEvent(idPassport, idEvent);
+        return true;
+    }
+
+    /**
+    * approveEvent
+    */
+    function cancelEvent(uint256 idPassport, uint idEvent)
+    public requireEventCreator(idPassport, idEvent) returns (bool)
+    {
+        _unlockToken(_passport[idPassport].waitingEvents.data[idEvent].sender, TOKEN_EVENT_LOCK);
+        _deleteWaitingEvent(idPassport, idEvent);
+        return true;
+    }
+
+    /**
+    * _deleteWaitingEvent
+    */
+    function _deleteWaitingEvent(uint256 idPassport, uint idToDelete)
+    private returns (bool)
+    {
+        //retreive idToDelete from index table
+        uint indexToDelete = 0;
+        for(uint i = 1; i < _passport[idPassport].waitingEvents.index.size+1; i++) {
+            if(_passport[idPassport].waitingEvents.index.data[i]==idToDelete){
+                indexToDelete = i;
+            }
+        }
+
+        //delete from index table
+        uint indexMax = _passport[idPassport].waitingEvents.index.size;
+        if(indexToDelete>0){
+            if(indexToDelete < indexMax){
+                _passport[idPassport].waitingEvents.index.data[indexToDelete] = _passport[idPassport].waitingEvents.index.data[indexMax];
+            }
+            delete(_passport[idPassport].waitingEvents.index.data[indexMax]);
+            _passport[idPassport].waitingEvents.index.size--;
+        }
+
+        //delete the waiting event
+        delete(_passport[idPassport].waitingEvents.data[idToDelete]);
     }
 
     /**
@@ -417,7 +560,7 @@ contract KarPassport {
     function balance(uint256 idPassport) public view returns (uint256) {
         return _passport[idPassport].balance;
     }
-    
+
     /**
      * balance
      */
@@ -467,29 +610,6 @@ contract KarPassport {
      */
     function modele(uint256 idPassport) public view returns (string memory) {
         return _passport[idPassport].identity.modele;
-    }
-
-
-
-
-
-
-
-
-    address[16] public adopters;
-    
-    // Adopting a pet
-    function adopt(uint petId) public returns (uint) {
-        require(petId >= 0 && petId <= 15);
-
-        adopters[petId] = msg.sender;
-
-        return petId;
-    }
-
-    // Retrieving the adopters
-    function getAdopters() public view returns (address[16] memory) {
-        return adopters;
     }
 
 
